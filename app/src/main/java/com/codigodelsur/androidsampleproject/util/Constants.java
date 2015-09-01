5bc06f64721d8aeef949e24712bdf8157c448bb9
Marcos Ambrosi <mambrosi@codigodelsur.com>
2015-09-01 17:51:10 -0300

    Added resources for repo

--- app/src/main/java/com/codigodelsur/androidsampleproject/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.codigodelsur.androidsampleproject.util;
-
-/**
- * Created by marcosambrosi on 9/1/15.
- */
-public class Constants {
-
-    public static final String BASE_URL = "https://pixabay.com";
-}
